8abca065db4e5f2838540738cb34044bc947210c
Konrad Łapka <konrad.lapka58@hotmail.com>
2022-12-09 17:06:22 +0100

    Update day 1

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -5,23 +5,23 @@ DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Elf:
     def __init__(self, file):
-        self.elves = {0: 0}
+        self.__elves = {0: 0}
         with open(file) as f:
             elf = 0
             for line in f:
                 if line.strip() == '':
                     elf += 1
-                    self.elves[elf] = 0
+                    self.__elves[elf] = 0
                 else:
-                    self.elves[elf] += int(line.strip())
+                    self.__elves[elf] += int(line.strip())
 
     @property
     def part1(self):
-        return sorted(self.elves.values())[-1]
+        return sorted(self.__elves.values())[-1]
 
     @property
     def part2(self):
-        return sum(sorted(self.elves.values())[-3:])
+        return sum(sorted(self.__elves.values())[-3:])
 
 
 def main():
