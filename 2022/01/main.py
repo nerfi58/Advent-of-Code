75d612005064dfac2fd8786b2834409d26792cfa
Konrad Łapka <konrad.lapka58@hotmail.com>
2022-12-09 03:06:38 +0100

    Add solution

--- 2022/01/main.py
+++ 2022/01/main.py
@@ -3,14 +3,31 @@ import os
 DIR = os.path.dirname(os.path.realpath(__file__))
 
 
-class Part1:
+class Elf:
     def __init__(self, file):
+        self.elves = {0: 0}
         with open(file) as f:
-            pass
+            elf = 0
+            for line in f:
+                if line.strip() == '':
+                    elf += 1
+                    self.elves[elf] = 0
+                else:
+                    self.elves[elf] += int(line.strip())
+
+    @property
+    def part1(self):
+        return sorted(self.elves.values())[-1]
+
+    @property
+    def part2(self):
+        return sum(sorted(self.elves.values())[-3:])
 
 
 def main():
-    part1 = Part1(os.path.join(DIR, 'input.txt'))
+    elf = Elf(os.path.join(DIR, 'input.txt'))
+    print(f"Part 1 solution: {elf.part1}")
+    print(f"Part 2 solution: {elf.part2}")
 
 
 if __name__ == '__main__':
